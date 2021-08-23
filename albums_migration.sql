ad034445edb73151689389c161f361cffd933e76
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-23 11:12:55 -0500

    completed

---
+++ albums_migration.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS  alblums;
+
+CREATE TABLE albums
+(
+    id           INT unsigned NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(100) NOT NULL,
+    name         VARCHAR(250) NOT NULL,
+    release_date YEAR,
+    sales        FLOAT,
+    genre        VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+describe albums;
